97abb1637964ac42969c0d365771e06d42116f21
TheFax <fabioslurp@yahoo.it>
2019-12-31 18:46:28 +0100

    First stages

--- shell/utilites.sh
+++ shell/utilites.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#!/bin/bash
 
 #remove a configuration line from a file
 sed -i '/Line to remove/d' /etc/file.conf
@@ -8,3 +9,11 @@ if [ -f /usr/file.txt ]; then
   rm -f /usr/file.txt
 fi
 
+#variable assegnation
+plain_ip_url="icanhazip.com"
+ip=$(wget -qO- $plain_ip_url)
+
+#loops forever:
+while (true); do
+  ...
+done
