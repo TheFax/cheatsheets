9b5b5584b6f10920fce04caa8aac9787b1831eef
TheFax <19174767+TheFax@users.noreply.github.com>
2021-03-27 08:25:28 +0100

    Create telegram_send.sh

---
+++ shell/telegram_send.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TOKEN="YOUR_TOKEN"
+CHAT_ID="YOUR_CHAT_ID"
+MESSAGE=$*
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+
+echo -e "SEND TELEGRAM NOTIFICATION..."
+
+while ! ping -c 1 8.8.8.8 | grep 'time=' > /dev/null ; do
+  echo -e ">   Waiting internet connection..."
+  sleep 1
+done
+echo -e ">   Internet connection found!"
+
+echo -e ">   Send telegram notification..."
+curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE" > /dev/null
