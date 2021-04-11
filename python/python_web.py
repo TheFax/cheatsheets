223d2b413f101c9e0a8f74f49f521d34951aaef6
TheFax <19174767+TheFax@users.noreply.github.com>
2021-04-11 15:24:10 +0200

    Update python_web.py

--- python/python_web.py
+++ python/python_web.py
@@ -1,12 +1,19 @@
+#pip install requests
+
 import requests
 
 url = 'http://myurl.com/request.php"
 
 data = {
-    'user' = 'John',
-    'id' = '12',
+    'user' : 'John',
+    'id'   : '12',
     }
 
-response = requests.post(url, data=data).text
+response = requests.post(url, data=data)
+#oppure
+response = requests.get(url)
 
-print(response)
+print(response.text)
+print(response.status_code)
+print(response.headers['content-type'])
+#     response... -> https://www.w3schools.com/python/ref_requests_response.asp
