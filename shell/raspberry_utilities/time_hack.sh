84c2428e1d1fd2dad94d5d65446e23bc379c5574
TheFax <19174767+TheFax@users.noreply.github.com>
2021-10-26 22:38:51 +0200

    Second version

--- shell/raspberry_utilities/time_hack.sh
+++ shell/raspberry_utilities/time_hack.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-#Regolo l'ora prendendo quella di Google
-sudo date -s $"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+SERVER="www.google.com"
 
-#Attivo regolazione automatica tramite ntp
-sudo timedatectl set-ntp true
+IS_SYNC="$(timedatectl status | grep 'System clock synchronized:')"
 
-#Visualizzo schermata riassuntiva
-timedatectl status
+if [[ $IS_SYNC=="System clock synchronized: yes" ]]
+then
+  echo "L'orario di sistema è già sincronizzato via NTP."
+  date
+  exit
+else
+  echo "L'orario di sistema non risulta sincronizzato."
+
+  echo "Setto l'orario rilevato dal server $SERVER"
+  #Regolo l'ora prendendo quella di Google
+  sudo date -s $"$(wget -qSO- --max-redirect=0 $SERVER 2>&1 | grep Date: | cut >
+
+  #Attivo regolazione automatica tramite ntp
+  sudo timedatectl set-ntp true
+
+  #Visualizzo schermata riassuntiva
+  timedatectl status
+fi
