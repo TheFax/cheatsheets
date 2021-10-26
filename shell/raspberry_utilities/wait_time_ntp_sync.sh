273db20350e95421339c11ee25139f740a548965
TheFax <19174767+TheFax@users.noreply.github.com>
2021-10-26 22:59:34 +0200

    First untested version

--- shell/raspberry_utilities/wait_time_ntp_sync.sh
+++ shell/raspberry_utilities/wait_time_ntp_sync.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+IS_SYNC="$(timedatectl status | grep 'System clock synchronized:')"
+
+if [[ $IS_SYNC == *"yes"* ]]
+then
+   echo "System time is sinchronized via NTP."
+else
+   echo "Waiting for system time sinchronization..."
+   IS_SYNC="$(timedatectl status | grep 'System clock synchronized:')"
+   while [[ $IS_SYNC == *"no"* ]]
+   do
+      sleep 1
+   done
+   echo "System time is now sinchronized via NTP."
+fi
