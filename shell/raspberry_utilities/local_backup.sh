c15d1c856e1774e4dec64d09962f171cbda066f1
TheFax <19174767+TheFax@users.noreply.github.com>
2022-04-04 14:42:00 +0200

    Update local_backup.sh

--- shell/raspberry_utilities/local_backup.sh
+++ shell/raspberry_utilities/local_backup.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+source_folder="./"
+destination_folder="../Local_backup/"
+prefix="Tendys Backup"
+
+actual_date=$(date +"%Y-%m-%d %H-%M-%S")
+mkdir "$destination_folder$actual_date - $prefix"
+cp -rv "$source_folder*" "$destination_folder$actual_date - $prefix"
