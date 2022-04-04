07ad165a88087878ac214098ef68b3bbfa54c22c
TheFax <19174767+TheFax@users.noreply.github.com>
2022-04-04 14:53:40 +0200

    Update remote_backup.sh

--- shell/raspberry_utilities/remote_backup.sh
+++ shell/raspberry_utilities/remote_backup.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+source_folder="./data_eater"
+destination_file="./data_eater_backup.zip"
+username=""
+ftp_address="ftp://ftp.back.altervista.org/"
+
+zip -r "$destination_file" "$source_folder"
+
+curl --netrc --upload-file "$destination_file" --user "$username" "$ftp_address"
+
+rm "$destination_file"
