25ae6fddb8b9b5a20e23e2c97fedaae17dbcab5a
TheFax <19174767+TheFax@users.noreply.github.com>
2023-01-16 15:37:09 +0100

    Appunti generici su List Tuple Set Dictionary

--- python/python.py
+++ python/python.py
@@ -153,6 +153,11 @@ while(i):
 
 
 
+-List       is a collection which is   ordered and   changeable.                  Allows duplicate members.
+-Tuple      is a collection which is   ordered and unchangeable.                  Allows duplicate members.
+-Set        is a collection which is unordered     unchangeable. unindexed            No duplicate members.
+-Dictionary is a collection which is   ordered and   changeable.                      No duplicate members.
+
 
 tupla = ("immutabile", 99, 'start from zero')
 print(tupla[1])
